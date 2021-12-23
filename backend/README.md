4c1403944df6aea78dea23347f0f51fef4be60ab
MdNazmul9 <nazmul.cse48@gmail.com>
2021-12-23 11:17:32 +0600

    docker and docker compose used

---
+++ backend/README.md
@@ -0,0 +1,38 @@
+# Django4_Basic
+
+## Run Using Docker
+  Place .env File First 
+#### Step 1 : First Install docker & docker-compose (if already installed then skip this step)
+```
+sudo apt update
+sudo apt upgrade
+sudo apt install docker.io
+```
+##### Install Docker compose
+```
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+```
+
+## Docker command text to run this project
+```
+docker build --tag python-django .
+```
+```
+docker run --publish 8000:8000 python-django
+```
+
+### Docker Basic Info
+![Docker Basic 1.1 ](https://github.com/MdNazmul9/Django4_Basic/blob/main/doc/1.1.png)
+![Docker Basic 1.2 ](https://github.com/MdNazmul9/Django4_Basic/blob/main/doc/1.2.png)
+
+
+# Run Project:
+  - 1. clone the project
+  - 2. 
+  ```
+  docker-compose build
+  docker-compose up
+  docker-compose down
+  ```
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-i8fktf5%%=3k+b*g)rf3=1(e$%6lq(0udl4np+x44c!5kw89l!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -87,24 +87,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'demodb',
-        'USER': 'nazmul',
-        'PASSWORD': '123456',
-        'HOST':'localhost',
-        'PORT': '5432',
-       }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'demodb',
+#         'USER': 'nazmul',
+#         'PASSWORD': '123456',
+#         'HOST':'localhost',
+#         'PORT': '5432',
+#        }
+# }
 
 
 
